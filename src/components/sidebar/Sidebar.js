6dae6513783c243fe1828388cf21575084b3df32
morihisashi <hisashi.mori@humanage.co.jp>
2023-03-29 16:20:18 +0900

    material Iconsのインストール

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import TwitterIcon from '@mui/icons-material/Twitter';
+
+function Sidebar() {
+    return (
+        <div classname="sidebar">
+            {/* ツイッターアイコン */}
+            <TwitterIcon classname="sidebar-twiterIcon" />
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* ツイートボタン */}
+        </div>
+    )
+}
+
+export default Sidebar
