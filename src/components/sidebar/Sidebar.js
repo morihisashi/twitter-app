ff61b6d7fc69c0f39e4b3447a316affb0a0ec071
morihisashi <hisashi.mori@humanage.co.jp>
2023-03-30 16:43:56 +0900

    sidebarの実装及びcss記載

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,19 +1,34 @@
-import React from 'react'
+import React from 'react';
+import SidebarOption from './SidebarOption';
 import TwitterIcon from '@mui/icons-material/Twitter';
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import { Button } from '@mui/material';
+import './Sidebar.css';
+
 
 function Sidebar() {
     return (
-        <div classname="sidebar">
+        <div className="sidebar">
             {/* ツイッターアイコン */}
-            <TwitterIcon classname="sidebar-twiterIcon" />
+            <TwitterIcon className="sidebar-twiterIcon" />
             {/* SidebarOption */}
-            {/* SidebarOption */}
-            {/* SidebarOption */}
-            {/* SidebarOption */}
-            {/* SidebarOption */}
-            {/* SidebarOption */}
-            {/* SidebarOption */}
+            <SidebarOption text="ホーム" Icon={HomeIcon} />
+            <SidebarOption text="話題の検索" Icon={SearchIcon} />
+            <SidebarOption text="通知" Icon={NotificationsNoneIcon} />
+            <SidebarOption text="メッセージ" Icon={MailOutlineIcon} />
+            <SidebarOption text="ブックマーク" Icon={BookmarkBorderIcon} />
+            <SidebarOption text="リスト" Icon={ListAltIcon} />
+            <SidebarOption text="プロフィール" Icon={PermIdentityIcon} />
+            <SidebarOption text="もっと見る" Icon={MoreHorizIcon} />
             {/* ツイートボタン */}
+            <Button variant="outlined" className="sidebar__tweet">ツイートする</Button>
         </div>
     )
 }
