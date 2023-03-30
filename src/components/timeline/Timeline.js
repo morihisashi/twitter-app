c2a5ea8703da85b07ad0a7732c51a37303474225
morihisashi <hisashi.mori@humanage.co.jp>
2023-03-30 17:10:27 +0900

    tweetboxのheader作成

---
+++ src/components/timeline/Timeline.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Timeline.css';
+
+function Timeline() {
+    return (
+        <div className="timeline">
+            {/* Header */}
+            <div className="timeline__header">
+                <h2>ホーム</h2>
+            </div>
+            {/* Tweetbox */}
+
+            {/* Post */}
+
+        </div>
+    )
+}
+
+export default Timeline
