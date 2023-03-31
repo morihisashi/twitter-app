161835d8b29c9a9adb45499ea36ea5e6c75c3662
morihisashi <hisashi.mori@humanage.co.jp>
2023-03-31 14:18:57 +0900

    tweetBoxの実装

--- src/components/timeline/Timeline.jsx
+++ src/components/timeline/Timeline.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Timeline.css';
+import TweetBox from './TweetBox';
 
 function Timeline() {
     return (
@@ -9,7 +10,7 @@ function Timeline() {
                 <h2>ホーム</h2>
             </div>
             {/* Tweetbox */}
-
+            <TweetBox />
             {/* Post */}
 
         </div>
