d45d5620b7c7e3066a28057531990943494bf647
morihisashi <hisashi.mori@humanage.co.jp>
2023-04-03 12:52:07 +0900

    .env.localにfirease情報を移行

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC9mO9mOJPHiBmBnZ26-brlHLq1HoV464s",
-    authDomain: "twitter-clone-udemy-36988.firebaseapp.com",
-    projectId: "twitter-clone-udemy-36988",
-    storageBucket: "twitter-clone-udemy-36988.appspot.com",
-    messagingSenderId: "353516920487",
-    appId: "1:353516920487:web:6c5c9f550ea8ce93698582"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_SENDER_ID
 };
 
 const app = initializeApp(firebaseConfig);
